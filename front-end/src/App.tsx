d46f50b2b8b250343cdacbc7f969b8d4daa934da
unknown <bfengos@gmail.com>
2020-02-04 14:43:14 -0500

    Boilerplate & Dependency Info

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Full Send</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

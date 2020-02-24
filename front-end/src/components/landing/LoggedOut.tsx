8940fc0ade31f839ecdcacfd75e81689d9f6aa30
Chris Sotirakos <chris.sotirakosx99@gmail.com>
2020-02-24 13:06:39 -0800

    Created the landing page for the website. I added visuals and pictures for the asthetics

--- front-end/src/components/landing/LoggedOut.tsx
+++ front-end/src/components/landing/LoggedOut.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
+
+
 export default function LoggedOut() {
   return (
-    <h1>Welcome to Full Send, please log in!</h1>
+    
+    <div>
+
+      
+<h1> Welcome to Full Send, please log in</h1>
+
+
+
+ 
+<h2>Post your pictures for millions of people to see</h2>
+<img src="/photo2.PNG" alt="photo2"/>
+
+ <img src="/photo4.PNG" alt="photo1"/>
+ <img src="/photo3.PNG" alt="photo1"/>
+
+<h2>Who is FullSend for?</h2>
+<h3>Anyone looking to make their photos appear on a world wide scale</h3>
+<ul>
+
+<li>
+  <h5>Post picture world wide</h5>
+</li>
+<li><h5>Follow friends and family</h5></li>
+<li><h5>Grow you network</h5></li>
+</ul>
+
+
+ <h2>Follow your friends, colleagues, classmates and family on FullSend</h2>
+ 
+    </div>
+  
+
   )
+
+  
+
 }
+

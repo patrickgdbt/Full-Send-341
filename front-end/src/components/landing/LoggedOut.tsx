5186b743030bcb82f9c3bfbc13a3c658d56bd70b
Chris Sotirakos <chris.sotirakosx99@gmail.com>
2020-02-24 19:34:09 -0800

    Updated the looks of the front page of the website, with more colors and pictures

--- front-end/src/components/landing/LoggedOut.tsx
+++ front-end/src/components/landing/LoggedOut.tsx
@@ -2,36 +2,50 @@ import React from 'react';
 
 
 
+
+
 export default function LoggedOut() {
   return (
     
+    
+    
     <div>
-
       
-<h1> Welcome to Full Send, please log in</h1>
+      
 
+<h1 style={{justifyContent:"center",alignItems:"center",display:"flex", color: 'White' }}>Welcome to Full Send, please log in</h1>
+
+
+<br></br>
 
 
  
-<h2>Post your pictures for millions of people to see</h2>
+<h2 style={{color: 'White'}}>Post your pictures for millions of people to see</h2>
 <img src="/photo2.PNG" alt="photo2"/>
 
  <img src="/photo4.PNG" alt="photo1"/>
  <img src="/photo3.PNG" alt="photo1"/>
 
-<h2>Who is FullSend for?</h2>
-<h3>Anyone looking to make their photos appear on a world wide scale</h3>
+<h1 style={{color: 'White'}}>Who is FullSend for?</h1>
+<h3 style={{color: 'White'}}>Anyone looking to make their photos appear on a world wide scale</h3>
 <ul>
 
 <li>
-  <h5>Post picture world wide</h5>
+  
+  <h4 style={{color: 'White'}}>Post picture world wide</h4>
 </li>
-<li><h5>Follow friends and family</h5></li>
-<li><h5>Grow you network</h5></li>
+<li><h4 style={{color: 'White'}}>Follow friends and family</h4></li>
+<li><h4 style={{color: 'White'}}>Grow you network</h4></li>
 </ul>
 
+<h3>
+<span>
+  <strong><h1 style={{color: 'White'}}>Wait..What is FullSend?</h1></strong>
+</span>
+</h3>
+<p><h4 style={{color: 'White'}}>Full Send will serve as a social media app, Instagram. The web app will allow users to create accounts, post images, comment on posts, follow other users, etc. In addition to the core features users may also be able to hashtag images, post stories, etc. Full Send will connect members within the community around you via images.</h4></p>
 
- <h2>Follow your friends, colleagues, classmates and family on FullSend</h2>
+ 
  
     </div>
   
